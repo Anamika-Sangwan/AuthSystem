eccbdf2343c67820a7753f0f697ae533bbd94e93
Anamika <anamika@Anamikas-MacBook-Pro.local>
2026-01-29 21:14:33 +0530

    Database added with entities, roles and tables

--- src/main/java/com/anamika/authsystem/AuthsystemApplication.java
+++ src/main/java/com/anamika/authsystem/AuthsystemApplication.java
@@ -5,6 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class AuthsystemApplication {
+
 	public static void main(String[] args) {
 		SpringApplication.run(AuthsystemApplication.class, args);
 	}
